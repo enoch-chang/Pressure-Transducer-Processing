6a239c0011d0bd03cfac1bb195b2754dbe9070f4
Enoch <ejh.chang@gmail.com>
2018-10-22 20:34:42 -0400

    Added docstring

--- handle_inputs.py
+++ handle_inputs.py
@@ -1,11 +1,21 @@
+"""
+Author: Enoch Chang
+
+Handling of main user input to handle the two main cases: single or multiple datasets
+"""
+
 import file_io
 import pressure_processor
 from datetime import datetime
 import os
 
 def handle_single(dir):
+    """ Handles case of when only one file is to be analyzed, executing
+    functions for data range selection, plotting and export
 
-    #filename = 'sample_1'
+    :param dir: Directory of where the data file is located
+    :return:
+    """
 
     filename = input("Filename? ")
 
@@ -24,7 +34,7 @@ def handle_single(dir):
     temp.show()
 
     try:
-        pressure,time = handle_interval_selection(pressure,time,start,end)
+        pressure,time = handle_interval_selection(pressure,time,end)
     except TypeError:
         return
 
@@ -37,12 +47,10 @@ def handle_single(dir):
 
     export_filename = save_dir + '/' + filename + ' ' + '.' + format
 
-    print (pressure[0],time[0])
-
     graph = pressure_processor.plot_single_pressure(pressure[0], time[0],
                                                     units)
 
-    pressure_processor.render_figure(graph,[filename],export_filename,format)
+    file_io.render_figure(graph,[filename],export_filename,format)
     pressure_processor.prepare_output_data(filename,save_dir,pressure[0],
                                            time[0])
 
@@ -50,6 +58,13 @@ def handle_single(dir):
 
 
 def handle_multi(dir, number_of_files):
+    """ Handles case of when more than one file is to be analyzed, executing
+    functions for data range selection, plotting and export
+
+    :param dir: Directory of where the data file is located
+    :param number_of_files: Number of files for analysis
+    :return:
+    """
 
     filename_list = input("Filenames? (Please separate using commas) ")
 
@@ -71,8 +86,20 @@ def handle_multi(dir, number_of_files):
 
     return
 
-def handle_interval_selection(pressure,time,start,end,new_pressure=[],
-                              new_time=[]):
+def handle_interval_selection(pressure,time,end,new_pressure=[],new_time=[]):
+    """ Facilitates selection of data range by requesting input, checking
+    validity of specified range and calling the function to modify the data
+    accordingly
+
+    :param pressure: Original pressure data list
+    :param time: Original time data points list
+    :param end: Final time point
+    :param new_pressure: Multidimensional array to store pressure
+    values of separate data sets
+    :param new_time: Multidimensional array to store time values of separate
+    data sets
+    :return: new_pressure and new_time lists, with selected range
+    """
 
     interval_input = input("Close the graph and enter the interval of "
                            "interest in seconds by specifying the start and "
